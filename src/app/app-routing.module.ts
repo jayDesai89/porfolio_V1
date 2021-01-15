fc797637be82fe76a2f8164e5c1f95c4ec520f40
Jay Desai <jaydesai1417@gmail.com>
2021-01-15 00:26:26 -0500

    Components created Header created Routing is set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PortfolioProjectComponent } from './components/portfolio-project/portfolio-project.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  /**
+   * If dashboard requires independent module then below line will apply lazy loading of "DashboardComponent"
+   * {path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule'},
+   */
+  {path: 'contactme', component: ContactMeComponent},
+  {path: 'portfolio', component: PortfolioProjectComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**', redirectTo: 'dashboard'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxSpinnerModule } from "ngx-spinner";
+// Service
+
+// Components
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PortfolioProjectComponent } from './components/portfolio-project/portfolio-project.component';
 import { AppComponent } from './app.component';
+
+// Pipes
+
+// Directives
+
+// Material
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    PortfolioProjectComponent,
+    ContactMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
